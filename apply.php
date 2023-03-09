16a3afa78a1945cca78119466082f5e14cbed5d6
은진 <80873499+eunjin2118@users.noreply.github.com>
2023-03-09 23:52:54 +0900

    db 연결

---
+++ apply.php
@@ -0,0 +1,15 @@
+<?php
+$uHakbun = $_POST["hakbun"];
+$uName = $_POST["name"];
+$uTel = $_POST["tel"];
+$uFiveme = $_POST["fiveme"];
+$uText = $_POST["text"];
+
+$conn = mysqli_connect('localhost', 'appandme2023', 'alflarhkgkrrh1!', 'appandme2023');
+
+$query = "insert into submit(name, student_id, five, motive, phone) values('$uName', '$uHakbun', '$uFiveme', '$uText', '$uTel');";
+mysqli_query($conn, $query);
+
+mysqli_close($conn);
+?>
+<meta http-equiv = "refresh" content="0;url-apply.html">
